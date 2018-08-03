a8eff41d8a1bda3a4a40ce996448385331db6c0b
liuJH <1471142959@qq.com>
2018-08-03 14:32:08 +0800

    add lris-spring

--- webs/lris-mybatis/src/main/java/com/lris/ain/test/Test.java
+++ webs/lris-mybatis/src/main/java/com/lris/ain/test/Test.java
@@ -3,7 +3,6 @@ package com.lris.ain.test;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.lris.ain.config.SessionFactoryConfiguration;
 import com.lris.ain.dao.BlogMapper;
