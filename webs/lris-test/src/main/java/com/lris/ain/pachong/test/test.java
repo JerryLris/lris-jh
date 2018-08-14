794cd3b06bf3d70b485bbec608c3496dd787fdd7
liuJH <1471142959@qq.com>
2018-08-14 14:26:06 +0800

    master conflix

--- webs/lris-test/src/main/java/com/lris/ain/pachong/test/test.java
+++ webs/lris-test/src/main/java/com/lris/ain/pachong/test/test.java
@@ -1,23 +1,10 @@
 package com.lris.ain.pachong.test;
 
-import java.util.Set;
-
-import com.lris.ain.pachong.core.Pachong;
-
 public class test {
 
 	public static void main(String[] args) throws Exception {
-		Pachong p = new Pachong();
-		String regex = "[a-zA-z]+://www.ugirls.com/Models/[^\\s]*.html";
-		for (int i=2;i<10;i++){
-			String url = "https://www.ugirls.com/Content/Page-"+ i +".html";
-			Set<String> set = p.getDowmLoadImageUrl(url, regex);
-			System.out.println("本网页下:"+url+"有"+set.size()+"个网页");
-            String regex2 = "[a-zA-z]+://img.ugirls.tv/uploads/magazine/content[^\\s]*.jpg";
-            for (String ul :set){
-            	p.doDowmLoadImageUrl(ul, regex2);
-            }
-		}
+		
+		System.out.println("这是我的master分支动的！");
 		
 		
 	}
